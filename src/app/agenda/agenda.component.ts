52ab5050e72500cbaae0d1482a2825b36a18ac6c
JsPinedas1 <js.pinedas1@uniandes.edu.co>
2024-03-11 18:36:12 -0500

    Generación README.md

--- src/app/agenda/agenda.component.ts
+++ src/app/agenda/agenda.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 
+import * as Constants from "../utils/Constants";
+
 @Component({
   selector: "app-agenda",
   templateUrl: "./agenda.component.html",
   styleUrls: ["./agenda.component.scss", "../app.component.scss"]
 })
 export class AgendaComponent {
+
+  CONSTANTS: any = Constants;
 
 }
--- src/app/alimentos/alimentos.component.ts
+++ src/app/alimentos/alimentos.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 
+import * as Constants from "../utils/Constants";
+
 @Component({
   selector: "app-alimentos",
   templateUrl: "./alimentos.component.html",
   styleUrls: ["./alimentos.component.scss", "../app.component.scss"]
 })
 export class AlimentosComponent {
+
+  CONSTANTS: any = Constants;
 
 }
